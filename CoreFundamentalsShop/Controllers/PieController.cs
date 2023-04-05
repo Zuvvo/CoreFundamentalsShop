5e385997869dbb94aad2495b3bbb175ffbd56880
Zuvvo <Zuvvo4@gmail.com>
2023-04-05 23:09:36 +0200

    added shopping cart, categories dropdown

--- CoreFundamentalsShop/Controllers/PieController.cs
+++ CoreFundamentalsShop/Controllers/PieController.cs
@@ -15,10 +15,24 @@ namespace CoreFundamentalsShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List()
+        public ViewResult List(string category)
         {
-            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "All pies");
-            return View(piesListViewModel);
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id)
