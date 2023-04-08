fea79867cd5933e03d1610b545b8f5b84763fb91
Zuvvo <Zuvvo4@gmail.com>
2023-04-08 11:36:03 +0200

    order form added

---
+++ CoreFundamentalsShop/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using CoreFundamentalsShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFundamentalsShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, IShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
--- CoreFundamentalsShop/Migrations/20230407153127_OrderAdded.Designer.cs
+++ CoreFundamentalsShop/Migrations/20230407153127_OrderAdded.Designer.cs
@@ -4,6 +4,7 @@ using CoreFundamentalsShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoreFundamentalsShop.Migrations
 {
     [DbContext(typeof(CoreFundamentalsShopDbContext))]
-    partial class CoreFundamentalsShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230407153127_OrderAdded")]
+    partial class OrderAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
